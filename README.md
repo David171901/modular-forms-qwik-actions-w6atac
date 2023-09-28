d0149ecf8dc8de6bec93541a0c65d5f4302f2b80
David171901 <91108331+David171901@users.noreply.github.com>
2023-09-27 22:33:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Qwik Playground
+
+The playground contains different forms to test the functionality of the library during development.
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ['"Lexend"', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
